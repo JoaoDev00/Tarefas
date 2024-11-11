d44f637f45bd7d8a091efcbcd0e3971c682c5821
João <poderosogud@gmail.com>
2024-11-11 15:17:06 -0300

    Angular teste

---
+++ src/app/components/usuario/usuarios-ficticios.ts
@@ -0,0 +1,32 @@
+export const usuarios = [
+  {
+      id: 'u1',
+      name: "Rafinha",
+      avatar: "user-1.jpg"
+  },
+  {
+      id: 'u2',
+      name: "Lipinho",
+      avatar: "user-2.jpg"
+  },
+  {
+      id: 'u3',
+      name: "Cariane",
+      avatar: "user-3.jpg"
+  },
+  {
+      id: 'u4',
+      name: "Julião",
+      avatar: "user-4.jpg"
+  },
+  {
+      id: 'u5',
+      name: "Maomau",
+      avatar: "user-5.jpg"
+  },
+  {
+      id: 'u6',
+      name: "Plasuleilo",
+      avatar: "user-6.jpg"
+  },
+]
